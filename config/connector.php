5cec2e1a05d9ab93df69c355ae8e063a4626707a
Micael Vinhas <mvinhas.tirso@gmail.com>
2018-12-05 18:48:19 +0000

    Fix #2 - Code MUST use an indent of 4 spaces, and MUST NOT use tabs for indenting

--- config/connector.php
+++ config/connector.php
@@ -7,30 +7,30 @@ define('DB_DATABASE', 'mvinhascms');
 #Connection creation
 class DBConnector
 {
-  private static $instance;
-  private $link;
-  private function __construct()
-  {
-    $this->connection = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
-  }
-
-  public static function init()
-  {
-    if(is_null(self::$instance)){
-      self::$instance = new DBConnector();
+    private static $instance;
+    private $link;
+    private function __construct()
+    {
+        $this->connection = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
     }
-    return self::$instance;
-  }
 
-  public function __call($name, $args)
-  {
-    if(method_exists($this->connection, $name)){
-      return call_user_func_array(array($this->connection,$name), $args);
-    } else {
-      trigger_error('Unknown Method ' . $name . '()', E_USER_WARNING);
-      return false;
+    public static function init()
+    {
+        if(is_null(self::$instance)){
+            self::$instance = new DBConnector();
+        }
+        return self::$instance;
     }
-  }
+
+    public function __call($name, $args)
+    {
+        if(method_exists($this->connection, $name)){
+            return call_user_func_array(array($this->connection,$name), $args);
+        } else {
+            trigger_error('Unknown Method ' . $name . '()', E_USER_WARNING);
+            return false;
+        }
+    }
 
 }
 ?>
--- engine/dbOperations.php
+++ engine/dbOperations.php
@@ -2,66 +2,65 @@
 
 class DBOperations
 {
-  protected $db;
-  public function __construct()
-  {
-      $this->db = dbConnector::init();
-  }
-
-  protected function fetchQuery($query_res)
-  {
-    $sql_fetch=array();
-    while($sql_retrieve = $query_res->fetch_assoc()){
-      $sql_fetch[] = $sql_retrieve;
-      
+    protected $db;
+    
+    public function __construct()
+    {
+        $this->db = dbConnector::init();
     }
-    return $sql_fetch;
-  }
 
-  public function create($dbname,$fields,$data)
-  {
-    $sql = $this->db->real_escape_string($this->db,"INSERT INTO $dbname ($fields) VALUES ($data)");
-    $sql_query = $this->db->query($sql);
-    $sql_query!= '' ? $message = 'Insert successful' : $message = 'Insert error!';
-    return $message;
-  }
-
-  public function selectAll($dbname)
-  {
-    $sql = $this->db->real_escape_string($this->db,"SELECT * FROM $dbname");
-    $sql_query = $this->db->query($sql);
-    $sql_fetch = $this->fetchQuery($sql_query);
-    return $sql_fetch;
-  }
-
-  public function select($dbname,$fields,$filter=''){
-    if ($filter==''){
-      $sql = $this->db->real_escape_string($this->db,"SELECT $fields FROM $dbname");
-    } else {
-      $sql = $this->db->real_escape_string($this->db,"SELECT $fields FROM $dbname WHERE $filter");
+    protected function fetchQuery($query_res)
+    {
+        $sql_fetch=array();
+        while($sql_retrieve = $query_res->fetch_assoc()){
+            $sql_fetch[] = $sql_retrieve;
+        }
+        return $sql_fetch;
     }
-    $sql_query = $this->db->query($sql);
-    $sql_fetch = $this->fetchQuery($sql_query);
-    return $sql_fetch;
-  }
 
-  public function update($dbname,$data,$condition){
-    $sql = $this->db->real_escape_string($this->db,"UPDATE $dbname SET $data WHERE $condition");
-    $sql_query = $this->db->query($sql);
-    $sql_query!= '' ? $message = 'Update successful' : $message = 'Update error!';
-    return $message;
-  }
-
-  public function delete($dbname,$condition=''){
-    if ($condition==''){
-      $sql = $this->db->real_escape_string($this->db,"DELETE * FROM $dbname");
-    } else {
-      $sql = $this->db->real_escape_string($this->db,"DELETE FROM $dbname WHERE $condition");
+    public function create($dbname,$fields,$data)
+    {
+        $sql = $this->db->real_escape_string($this->db,"INSERT INTO $dbname ($fields) VALUES ($data)");
+        $sql_query = $this->db->query($sql);
+        $sql_query!= '' ? $message = 'Insert successful' : $message = 'Insert error!';
+        return $message;
     }
-    $sql_query = $this->db->query($sql);
-    $sql_query!= '' ? $message = 'Delete successful' : $message = 'Delete error!';
-    return $message;
-  }
 
+    public function selectAll($dbname)
+    {
+        $sql = $this->db->real_escape_string($this->db,"SELECT * FROM $dbname");
+        $sql_query = $this->db->query($sql);
+        $sql_fetch = $this->fetchQuery($sql_query);
+        return $sql_fetch;
+    }
+
+    public function select($dbname,$fields,$filter=''){
+        if ($filter==''){
+            $sql = $this->db->real_escape_string($this->db,"SELECT $fields FROM $dbname");
+        } else {
+            $sql = $this->db->real_escape_string($this->db,"SELECT $fields FROM $dbname WHERE $filter");
+        }
+        $sql_query = $this->db->query($sql);
+        $sql_fetch = $this->fetchQuery($sql_query);
+        return $sql_fetch;
+    }
+
+    public function update($dbname,$data,$condition){
+        $sql = $this->db->real_escape_string($this->db,"UPDATE $dbname SET $data WHERE $condition");
+        $sql_query = $this->db->query($sql);
+        $sql_query!= '' ? $message = 'Update successful' : $message = 'Update error!';
+        return $message;
+    }
+
+    public function delete($dbname,$condition=''){
+        if ($condition==''){
+            $sql = $this->db->real_escape_string($this->db,"DELETE * FROM $dbname");
+        } else {
+            $sql = $this->db->real_escape_string($this->db,"DELETE FROM $dbname WHERE $condition");
+        }
+        $sql_query = $this->db->query($sql);
+        $sql_query!= '' ? $message = 'Delete successful' : $message = 'Delete error!';
+        return $message;
+    }
 }
 ?>
