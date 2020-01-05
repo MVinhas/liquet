f79607606980bd0d6900246c18fb6d75b8f28d3a
Micael Vinhas <mvinhas.tirso@gmail.com>
2020-01-05 13:42:58 +0000

    Added support for nginx homedir, at the same time the old .htaccess was removed

--- config/Dispatcher.php
+++ config/Dispatcher.php
@@ -6,8 +6,15 @@ class Dispatcher
     public static function dispatch()
     {
         $url = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
+        
         array_shift($url);
-
+        $siteInfo = new \engine\SiteInfo();
+        
+        //if using HomeDir, the second position is the site name, not the controller name
+        if ($url[0] == $siteInfo->getName()) {
+            array_shift($url);
+        }
+         
         //check for controller
         $controller = !empty($url[0]) ? "\controllers\\" . $url[0] . 'Controller' : '\controllers\HomeController';
 
