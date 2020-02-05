959cb45103375aefaf1db9e27ec60b41a248a231
Micael Vinhas <mvinhas.tirso@gmail.com>
2020-02-05 21:37:17 +0000

    Admin suite backbone

---
+++ controllers/AdminController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace controllers;
+
+use \models\Admin as Admin;
+
+class AdminController extends Controller
+{
+    protected $path;
+    
+    public function __construct()
+    {
+        parent::__construct();
+        $file = pathinfo(__FILE__, PATHINFO_FILENAME);
+        $this->path = $this->getDirectory($file);
+    }
+
+    public function index()
+    {
+        if (!isset($_SESSION['user']))
+            $this->login();
+    }
+    
+    public function login()
+    {
+        $out = array();
+        $out['debug_mode'] = $this->config_flags->debug_mode;
+        $loginTemplate = $this->getFile($this->path, __FUNCTION__);
+        echo $this->callTemplate($loginTemplate, $out);
+    }
+
+    public function createSession()
+    {
+        $admin = new Admin();
+        $user = $admin->getUser($_POST['email']);
+        if ($user === true)
+            $_SESSION['user'] = $_POST;
+        echo "<pre>";print_r($_SESSION);"</pre>";
+    }
+}
---
+++ models/Admin.php
@@ -0,0 +1,24 @@
+<?php
+    namespace models;
+
+    use \engine\DbOperations as DbOperations;
+    use \controllers\AdminController as AdminController;
+    
+    class Admin
+    {
+        protected $db; 
+        public function __construct()
+        {
+            $this->db = new DbOperations;
+        }
+        
+
+        public function getUser($email)
+        {
+            $user = $this->db->select('users','id','email = '.$email);
+            if (isset($user))
+                return true;
+            return false;
+        }
+           
+    }
