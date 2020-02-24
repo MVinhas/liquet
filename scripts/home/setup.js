7fcf0d8d44f3efe0c4abdbcd2ed1cfdbcf305e58
Micael Vinhas <dev.mvinhas@gmail.com>
2020-02-24 16:59:57 +0000

    Registration form control

--- scripts/home/setup.js
+++ scripts/home/setup.js
@@ -1,3 +1,19 @@
+$("#validationCustomPassword").on("focusout", function () {
+  if ($(this).val() != $("#password2").val()) {
+      $("#validationCustomPassword2").removeClass("is-valid").addClass("is-invalid");
+  } else {
+      $("#validationCustomPassword2").removeClass("is-invalid").addClass("is-valid");
+  }
+  });
+  
+  $("#validationCustomPassword2").on("keyup", function () {
+  if ($("#validationCustomPassword").val() != $(this).val()) {
+      $(this).removeClass("is-valid").addClass("is-invalid");
+  } else {
+      $(this).removeClass("is-invalid").addClass("is-valid");
+  }
+});
+
 (function() {
     'use strict';
     window.addEventListener('load', function() {
@@ -5,8 +21,17 @@
       var forms = document.getElementsByClassName('needs-validation');
       // Loop over them and prevent submission
       var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('keypress', function(event) {
+          if (form.checkValidity() === true) {
+            $('#submit').prop("disabled", false); 
+          } else {
+            $('#submit').prop("disabled", true); 
+          }
+        });
         form.addEventListener('submit', function(event) {
+          
           if (form.checkValidity() === false) {
+            
             event.preventDefault();
             event.stopPropagation();
           }
