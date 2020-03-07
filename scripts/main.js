c1d321d8d14a68d3413903c48274615a25ae585d
Micael Vinhas <dev.mvinhas@gmail.com>
2020-03-07 16:01:21 +0000

    Added global javascript file

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+/**
+ * Validates if a password has at least 6 characters
+ * @param {*} inputtxt 
+ */
+function CheckPassword(inputtxt) 
+{ 
+    console.log(inputtxt);
+    var passw = /^.{6,}$/;
+    if(inputtxt.match(passw)) { 
+        return true;
+    } else { 
+        return false;
+    }
+}
