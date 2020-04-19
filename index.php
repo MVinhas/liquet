1db80432c5d19f653692d2140ef05d57ce310228
Micael Vinhas <dev.mvinhas@gmail.com>
2020-04-19 12:35:45 +0100

    Debug data easier to reach on index

--- index.php
+++ index.php
@@ -8,3 +8,17 @@ require_once 'config/conf.php';
 
 $site = new \controllers\SiteController;
 $site->index();
+if ($config_flags->debug_mode === 1) {
+    echo "<pre>";
+    echo "<b>SESSION:</b>";
+    print_r($_SESSION);
+    echo "<br><b>POST:</b>";
+    print_r($_POST);
+    echo "<br><b>GET:</b>";
+    print_r($_GET);
+    echo "<br><b>COOKIE:</b>";
+    print_r($_COOKIE);
+    echo "<br><b>SERVER:</b>";
+    print_r($_SERVER);
+    echo "</pre>";
+}
