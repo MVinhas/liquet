f433491d3e056bb378e4d5e0478779404c46a2b1
Micael Vinhas <dev.mvinhas@gmail.com>
2020-04-21 09:27:22 +0100

    Debug things had been moved for cleaniness purposes

---
+++ debug.php
@@ -0,0 +1,13 @@
+<?php   
+    echo "<pre>";
+    echo "<b>SESSION:</b>";
+    print_r($_SESSION);
+    echo "<br><b>POST:</b>";
+    print_r($_POST);
+    echo "<br><b>GET:</b>";
+    print_r($_GET);
+    echo "<br><b>COOKIE:</b>";
+    print_r($_COOKIE);
+    echo "<br><b>SERVER:</b>";
+    print_r($_SERVER);
+    echo "</pre>";
