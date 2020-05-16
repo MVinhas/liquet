1a1dfed3990fe102f18b830d6525aaa009ec639f
Micael Vinhas <dev.mvinhas@gmail.com>
2020-05-16 10:39:46 +0100

    Removed post create since the same template will be used for both create and edit posts

--- engine/DbOperations.php
+++ engine/DbOperations.php
@@ -21,7 +21,11 @@ class DbOperations
 
     public function create($table, $fields, $data)
     {
-        $data_array = explode(',', $data);
+        if (is_array($data)) {
+            $data_array = array_values($data);
+        } else {
+            $data_array = explode(',', $data);
+        }
         $prepare_array = array();
         foreach ($data_array as $k => $v) {
             $prepare_array[$k] = str_replace($v, '?', $data_array[$k]);
@@ -87,10 +91,21 @@ class DbOperations
     }
 
     public function update($table, $fields, $fields_value, $where, $where_value)
-    {
-        $sql = "UPDATE $table SET $fields WHERE $condition";
-        $data_array = explode(',', $fields_value);
-        
+    {   
+        if (is_array($fields_value)) {
+            $data_array = array_values($fields_value);
+        } else {
+            $data_array = explode(',', $fields_value);
+        }
+
+        if (is_array($where_value)) {
+            $data_array_where = array_values($where_value);
+        } else {
+            $data_array_where = explode(',', $where_value);
+        }
+
+        $sql = "UPDATE $table SET $fields WHERE $where";
+
         $count_fields = substr_count($fields, '?');
         $values = array();
         $values_type = array();
@@ -105,16 +120,15 @@ class DbOperations
         $count_fields_where = substr_count($where, '?');
         $values_where = array();
         $values_type_where = array();
-        for ($i=0; $i < $count_fields; $i++) {
-            $field{$i} = ltrim($data_array[$i], ' ');
+        for ($i=0; $i < $count_fields_where; $i++) {
+            $field{$i} = ltrim($data_array_where[$i], ' ');
             
-            $values_type_where{$i} = strtolower(substr(gettype($data_array[$i]), 0, 1));
+            $values_type_where{$i} = strtolower(substr(gettype($data_array_where[$i]), 0, 1));
             array_push($values_where, $field{$i});
         }
         $values_type_where = implode('', $values_type_where);
-
+ 
         $values_type = $values_type.$values_type_where;
-
         $sql = $this->db->prepare($sql);
         $sql->bind_param("$values_type", ...$values, ...$values_where);
 
