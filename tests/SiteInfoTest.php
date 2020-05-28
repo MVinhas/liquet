030cc684ded842dbc2bdce5fcedbe2e122c3394f
Micael Vinhas <dev.mvinhas@gmail.com>
2020-05-28 22:06:54 +0100

    Test if flags is an object

--- tests/SiteInfoTest.php
+++ tests/SiteInfoTest.php
@@ -5,6 +5,11 @@ use PHPUnit\Framework\TestCase;
 class SiteInfoTest extends TestCase
 {
 
+    public function testConfigFlagsIsObject()
+    {
+        $siteInfo = new \engine\SiteInfo;
+        $this->assertIsObject($siteInfo->flags);
+    }
     public function testReturnsAlwaysASiteName()
     {
         $siteInfo = new \engine\SiteInfo;
