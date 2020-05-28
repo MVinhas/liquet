9d4fcc587bc8b35b309a83254b8bb9cb31ef6123
Micael Vinhas <dev.mvinhas@gmail.com>
2020-05-28 22:04:20 +0100

    Tested the SiteInfo Class

---
+++ tests/SiteInfoTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class SiteInfoTest extends TestCase
+{
+
+    public function testReturnsAlwaysASiteName()
+    {
+        $siteInfo = new \engine\SiteInfo;
+        $this->assertNotEquals('', $siteInfo->getName());
+    }
+
+    public function testReturnsAlwaysASiteVersion()
+    {
+        $siteInfo = new \engine\SiteInfo;
+        $this->assertNotEquals('', $siteInfo->getVersion());
+    }
+
+    public function testReturnsAlwaysASiteAuthor()
+    {
+        $siteInfo = new \engine\SiteInfo;
+        $this->assertNotEquals('', $siteInfo->getAuthor());
+    }
+
+    public function testReturnsAlwaysASiteLaunchYear()
+    {
+        $siteInfo = new \engine\SiteInfo;
+        $this->assertNotEquals('', $siteInfo->getLaunchYear());
+    }
+
+    public function testCopyrightNotEmpty()
+    {
+        $siteInfo = new \engine\SiteInfo;
+        $this->assertNotEquals('', $siteInfo->getCopyright());
+        $this->assertNotEquals('-', $siteInfo->getCopyright());
+    }
+
+}
