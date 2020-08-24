a61c5da0c9596dbab02db18d5177e0a92cc1adca
Micael Vinhas <dev.mvinhas@gmail.com>
2020-08-24 19:22:06 +0100

    Simplified debug file

--- debug.php
+++ debug.php
@@ -1,11 +1,6 @@
-<?php   
-    echo "<pre style='z-index: 100;
-    position: absolute;
-    border: 1px solid #ccc;
-    top: 100%;
-    background: #eee;
-    width: 100%;
-    left:0;'>";
+
+<?php
+    echo "<pre class='debug'>";
     echo "<b>SESSION:</b>";
     print_r($_SESSION);
     echo "<br><b>POST:</b>";
