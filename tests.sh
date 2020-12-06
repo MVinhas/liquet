bc80b377cab80bf4ce909f034c17ac968c7c1564
Micael Vinhas <dev.mvinhas@gmail.com>
2020-12-06 10:35:46 +0000

    New test: CPanelTest

--- tests.sh
+++ tests.sh
@@ -1,3 +1,4 @@
 ./vendor/bin/phpunit --bootstrap config/conf.php tests/ControllerTest.php
 ./vendor/bin/phpunit --bootstrap config/conf.php tests/AdminTest.php
 ./vendor/bin/phpunit --bootstrap config/conf.php tests/SiteInfoTest.php
+./vendor/bin/phpunit --bootstrap config/conf.php tests/CPanelTest.php
