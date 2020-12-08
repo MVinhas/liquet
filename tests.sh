defadb23d745cafb376c85e30a84de8e2fc7a971
Micael Vinhas <dev.mvinhas@gmail.com>
2020-12-08 14:07:40 +0000

    New tests

--- tests.sh
+++ tests.sh
@@ -1,4 +1,11 @@
 ./vendor/bin/phpunit --bootstrap config/conf.php tests/ControllerTest.php
+
 ./vendor/bin/phpunit --bootstrap config/conf.php tests/AdminTest.php
-./vendor/bin/phpunit --bootstrap config/conf.php tests/SiteInfoTest.php
 ./vendor/bin/phpunit --bootstrap config/conf.php tests/CPanelTest.php
+./vendor/bin/phpunit --bootstrap config/conf.php tests/HeaderTest.php
+./vendor/bin/phpunit --bootstrap config/conf.php tests/HomeTest.php
+./vendor/bin/phpunit --bootstrap config/conf.php tests/PostTest.php
+./vendor/bin/phpunit --bootstrap config/conf.php tests/SiteTest.php
+
+./vendor/bin/phpunit --bootstrap config/conf.php tests/SiteInfoTest.php
+./vendor/bin/phpunit --bootstrap config/conf.php tests/DbOperationsTest.php
