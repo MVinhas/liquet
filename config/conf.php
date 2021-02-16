91cb9cbeb1d6b5fd4e99bd32b3632aefbe1bd34f
Micael Vinhas <dev.mvinhas@gmail.com>
2021-02-16 11:52:15 +0000

    Added configuration from database

--- config/conf.php
+++ config/conf.php
@@ -1,23 +1,6 @@
 <?php
 if (!defined('OWNER')) define('OWNER', 'dev.mvinhas@gmail.com');
 
-$config_flags = new stdClass();
-$config_flags->debug_mode = 1; #0 - Production; 1 - Development
-$config_flags->sitename = 'mvinhas-blog';
-$config_flags->email = 'dev.mvinhas@gmail.com';
-$config_flags->siteversion = '0.1.0-alpha5';
-$config_flags->siteauthor = 'Micael Vinhas';
-$config_flags->launchyear = '2019';
-
-if ($config_flags->debug_mode === 1) {
-    $time_start = microtime(true);
-    ini_set("display_errors", "On");
-    ini_set("error_prepend_string","<div class='debug-silent'>");
-    ini_set("error_append_string","</div>");
-    mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_STRICT); 
-    error_reporting(E_ALL);   
-}
-
 require_once 'Connector.php';
 
 require_once 'autoloader.php';
@@ -27,6 +10,27 @@ spl_autoload_register('autoload');
 require_once 'vendor/autoload.php';
 
 require_once 'engine/DbOperations.php';
+
+$config_flags = new \engine\DbOperations();
+$__CONFIG = $config_flags->select('config', '*');
+if (empty($__CONFIG))
+    trigger_error("No Config found. Please check your DB Connection");
+
+$config_flags->debug_mode = $__CONFIG['debug_mode'];
+$config_flags->sitename = $__CONFIG['sitename'];
+$config_flags->email = $__CONFIG['email'];
+$config_flags->siteversion = $__CONFIG['siteversion'];
+$config_flags->siteauthor = $__CONFIG['siteauthor'];
+$config_flags->launchyear = $__CONFIG['launchyear'];
+
+if ((int)$__CONFIG['debug_mode'] === 1 || empty($__CONFIG)) {
+    $time_start = microtime(true);
+    ini_set("display_errors", "On");
+    ini_set("error_prepend_string","<div class='debug-silent'>");
+    ini_set("error_append_string","</div>");
+    mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_STRICT); 
+    error_reporting(E_ALL);   
+}
 
 $loader = new \Twig\Loader\FilesystemLoader('views');
 
