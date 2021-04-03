a32bf28a12fc6048c4462044df791990190a070a
Micael Vinhas <dev.mvinhas@gmail.com>
2021-04-03 15:35:45 +0100

    DummyTest for annotations

---
+++ tests/DummyTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class DbOperationsTest extends TestCase
+{
+    /**
+     * These are not tests, these are only annotations 
+     * that can be useful in future tests
+     */
+
+     /**
+      * public function testNewQueueIsEmpty()
+      {
+          $queue = new Queue;
+
+          $this->assertEquals(0, $queue->getCount());
+
+          return $queue;
+      } 
+      
+      we just need to put a @depend before the class
+      this way we don't have to instanciate Queue again
+
+
+      @depends testNewQueueIsEmpty
+      public function testAnItemIsAddedToTheQueue(Queue $queue)
+      {
+          //code goes here
+      }
+      */
+}
