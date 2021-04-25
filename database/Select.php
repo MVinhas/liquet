704cff559e89f203a6594b4f995e44fadcc900d8
Micael Vinhas <dev.mvinhas@gmail.com>
2021-04-25 11:14:10 +0100

    Select query improved

--- database/Select.php
+++ database/Select.php
@@ -3,17 +3,37 @@ namespace database;
 
 class Select
 {
-    public $fields;
-
     public $table;
 
-    private $where;
+    public $fields;
 
-    public function __construct(array $fields, string $table)
+    public function __construct($table)
     {
-        $this->fields = implode(', ', $fields);
+        $this->table = $table;    
+    }
 
-        $this->table = $table;
+    public static function table(string $table)
+    {
+        return new Select($table);
+    }
+
+    public function select(...$fields)
+    {
+        $this->fields = $fields;
+        return $this;
+    }
+
+    public function queryBuilder()
+    {
+        $query = array();
+
+        $query[] = "SELECT ".implode(', ',$this->fields)." FROM ".$this->table;
+        
+        if (!empty($this->where)) {
+            $query[] = "WHERE ".implode(" AND ", $this->where);
+        }
+
+        return implode(' ', $query);
     }
 
     public function where(array $condition)
@@ -26,20 +46,10 @@ class Select
         return $this;
     }
 
-    public function queryBuilder()
-    {
-        $query[] = "SELECT ".$this->fields." FROM ".$this->table;
-        
-        if (!empty($this->where)) {
-            $query[] = "WHERE ".implode(" AND ", $this->where);
-        }
-
-        return implode(' ', $query);
-    }
-    
     public function raw()
     {
         return $this->queryBuilder();
     } 
+    
 
 }
