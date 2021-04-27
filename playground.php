a00a407b7eb972b3c407a1687e21841a80c31ef2
Micael Vinhas <dev.mvinhas@gmail.com>
2021-04-27 14:53:13 +0100

    Playground not ignored; can be useful to show examples

---
+++ playground.php
@@ -0,0 +1,29 @@
+<?php
+namespace Database;
+
+use \Database\Select;
+use \Database\Create;
+use \Database\Update;
+use \Database\Insert;
+use \Database\Delete;
+
+require 'config/conf.php';
+
+ini_set("display_errors", "On");
+error_reporting(E_ALL);   
+
+echo Create::table('users')->set([
+    'id' => 'INT NOT NULL AUTO_INCREMENT',
+    'name' => 'VARCHAR(64) NOT NULL'
+])->raw();
+echo "<br>";
+echo Select::table('users')->fields('id', 'name')->where(['name' => 'Catarina'])->raw();
+echo "<br>";
+echo Update::table('users')->set(['name' => 'Filipa'])->where(['id' => 1])->raw();
+echo "<br>";
+echo Insert::table('users')->set([
+    'name' => 'Joana',
+    'age' => 30
+])->raw();
+echo "<br>";
+echo Delete::table('users')->where(['name' => 'Joana'])->raw();
