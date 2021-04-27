23c9b30af9ab15218072a2eb49fd73a7d9717c4b
Micael Vinhas <dev.mvinhas@gmail.com>
2021-04-27 17:02:06 +0100

    reopen #7

--- engine/SiteInfo.php
+++ engine/SiteInfo.php
@@ -8,35 +8,51 @@ class SiteInfo
     public function __construct()
     {
         global $config_flags;
-        return $this->flags ?? new \StdClass;
+        if (is_object($config_flags))
+            $this->flags = $config_flags;
+        else
+            $this->flags = new \StdClass;
     }
 
     public function getName()
     {
-        return $this->flags() ?? trim($this->flags->sitename) ?? '(no name found)';
+        if (isset($this->flags->sitename)) 
+            return trim($this->flags->sitename);
+        else
+            return '(no name found)';
     }
 
     public function getVersion()
     {
-        return $this->flags() ?? trim($this->flags->siteversion) ?? '(no version found)';
+        if (isset($this->flags->siteversion)) 
+            return trim($this->flags->siteversion);
+        else
+            return '(no version found)';
     }
 
     public function getAuthor()
     {
-        return $this->flags() ?? trim($this->flags->siteauthor) ?? '(no author found)';
+        if (isset($this->flags->siteauthor)) 
+            return trim($this->flags->siteauthor);
+        else
+            return '(no author found)';
     }
 
     public function getLaunchYear()
     {
-        return $this->flags() ?? trim($this->flags->launchyear) ?? date('Y');
+        if (isset($this->flags->launchyear)) 
+            return trim($this->flags->launchyear);
+        else
+            return date('Y');
     }
 
     public function getCopyright()
     {
         if ($this->getLaunchYear() >= date('Y')) {
-            return $this->flags() ?? trim($this->getLaunchYear().' '.$this->getAuthor());
+            $copyright = trim($this->getLaunchYear().' '.$this->getAuthor());
+        } else {
+            $copyright = trim($this->getLaunchYear().' - '.date('Y').' '.$this->getAuthor());
         }
-
-        return $this->flags() ?? trim($this->getLaunchYear().' - '.date('Y').' '.$this->getAuthor());
+        return $copyright;
     }
 }
