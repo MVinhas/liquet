30727de03275bf163b077ba990f74da11ce5b964
Micael Vinhas <dev.mvinhas@gmail.com>
2021-04-27 16:34:38 +0100

    Fix #7 - Use of Null Coalescing

--- engine/SiteInfo.php
+++ engine/SiteInfo.php
@@ -8,51 +8,35 @@ class SiteInfo
     public function __construct()
     {
         global $config_flags;
-        if (is_object($config_flags))
-            $this->flags = $config_flags;
-        else
-            $this->flags = new \StdClass;
+        return $this->flags ?? is_object($config_flags) ?? new \StdClass;
     }
 
     public function getName()
     {
-        if (isset($this->flags->sitename)) 
-            return trim($this->flags->sitename);
-        else
-            return '(no name found)';
+        return $sitename ?? trim($this->flags->sitename) ?? '(no name found)';
     }
 
     public function getVersion()
     {
-        if (isset($this->flags->siteversion)) 
-            return trim($this->flags->siteversion);
-        else
-            return '(no version found)';
+        return $siteversion ?? trim($this->flags->siteversion) ?? '(no version found)';
     }
 
     public function getAuthor()
     {
-        if (isset($this->flags->siteauthor)) 
-            return trim($this->flags->siteauthor);
-        else
-            return '(no author found)';
+        return $siteauthor ?? trim($this->flags->siteauthor) ?? '(no author found)';
     }
 
     public function getLaunchYear()
     {
-        if (isset($this->flags->launchyear)) 
-            return trim($this->flags->launchyear);
-        else
-            return date('Y');
+        return $launchyear ?? trim($this->flags->launchyear) ?? date('Y');
     }
 
     public function getCopyright()
     {
         if ($this->getLaunchYear() >= date('Y')) {
-            $copyright = trim($this->getLaunchYear().' '.$this->getAuthor());
-        } else {
-            $copyright = trim($this->getLaunchYear().' - '.date('Y').' '.$this->getAuthor());
+            return trim($this->getLaunchYear().' '.$this->getAuthor());
         }
-        return $copyright;
+
+        return trim($this->getLaunchYear().' - '.date('Y').' '.$this->getAuthor());
     }
 }
